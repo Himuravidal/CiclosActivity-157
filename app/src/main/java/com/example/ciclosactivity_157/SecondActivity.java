24e688dec363a0758e957cdbce81396178a0f709
Cristian Vidal <Cristian.vidal.lopez@gmail.com>
2020-12-17 10:10:32 -0300

    ADD: realize intent implicito en la segunda actividad

--- app/src/main/java/com/example/ciclosactivity_157/SecondActivity.java
+++ app/src/main/java/com/example/ciclosactivity_157/SecondActivity.java
@@ -2,18 +2,37 @@ package com.example.ciclosactivity_157;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
     private String TAG = SecondActivity.class.getSimpleName();
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        button = findViewById(R.id.button_second);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Intent Implicito a el navegador
+                String url = "https://www.youtube.com";
+                Intent mIntent = new Intent(Intent.ACTION_VIEW);
+                mIntent.setData(Uri.parse(url));
+                startActivity(mIntent);
+            }
+        });
+
     }
 
     @Override
